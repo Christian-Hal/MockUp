cf645426a51d00698135956c0d251b764b160521
Gavin Dominique <gavindom315@gmail.com>
2026-03-23 08:02:55 -0500

    small change to allow me to use the cross brush during the demo

--- include/BrushManager.h
+++ include/BrushManager.h
@@ -34,7 +34,7 @@ class BrushManager
         std::vector<BrushTool> loaded_Brushes;
 
         // list of all default brush paths to load on init
-        const std::vector<std::string> defaultBrushPaths = {"circle.gbr", "cross.gbr", "confetti.gbr"};
+        const std::vector<std::string> defaultBrushPaths = {"circle.gbr", "confetti.gbr"}; //"cross.gbr"
 
         // index of the active brush in loaded_Brushes
         int activeBrushIndex = 0;
