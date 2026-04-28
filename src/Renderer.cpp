0d3f394ffad8c2a51d166d8eb7dbd41896b95728
Gavin Dominique <gavindom315@gmail.com>
2026-04-27 22:06:38 -0500

    zoom mipmap fix

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -115,7 +115,7 @@ bool Renderer::init(GLFWwindow* window, Globals& g_inst)
 
 	glBindTexture(GL_TEXTURE_2D, canvasTexture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
@@ -320,6 +320,14 @@ void Renderer::renderCanvas(const Canvas& canvas)
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, canvasTexture);
+
+	// check to see how the mag filter should be handled based on the zoom level
+	if (canvas.zoom < 0.5f) {
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	} else {
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	}
+
 	glUseProgram(shaderProgram);
 	glUniform1i(glGetUniformLocation(shaderProgram, "canvasTex"), 0);
 
